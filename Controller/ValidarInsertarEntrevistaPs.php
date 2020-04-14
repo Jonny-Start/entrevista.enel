1846b346c8ab1889f7d4e699204f3b01a2543838
JonnyStart0910 <58595053+JonnyStart0910@users.noreply.github.com>
2020-04-13 22:06:04 -0500

    Primer Commit

---
+++ Controller/ValidarInsertarEntrevistaPs.php
@@ -0,0 +1,25 @@
+<?php 
+require_once "../Model/conexioon.php";
+extract($_REQUEST);
+
+
+$fecha=date("Y-m-d",strtotime ("$_REQUEST[fechaEntrevista]"));
+
+
+$sql= "INSERT INTO  entrevistapsicologica (nombre, cc, fecha_nac, dependencia, cargo, cargoAspira, telefono, aspiracionSal,
+ fechaEntrevista, psicologa, responsabilidad,productividad, gestionCambio,
+  innovacion, resiliencia, confianza, comportamientoEtico, fuenteR, resultadoI, inteligenciaE,
+   candidatoU, explicacion, resolucionProblemas, observaciones, adecuado, adecuadoP, parcialmente, primera, segunda, noA, concepto)
+VALUES('$_REQUEST[nombre]','$_REQUEST[cc]','$_REQUEST[fecha_nac]','$_REQUEST[dependencia]','$_REQUEST[cargo]','$_REQUEST[cargoAspira]','$_REQUEST[telefono]','$_REQUEST[aspiracionSal]','$fecha' ,'$_REQUEST[psicologa]','$_REQUEST[responsabilidad]','$_REQUEST[productividad]','$_REQUEST[gestionCambio]','$_REQUEST[innovacion]','$_REQUEST[resiliencia]','$_REQUEST[confianza]','$_REQUEST[comportamientoEtico]','$_REQUEST[fuenteR]','$_REQUEST[resultadoI]','$_REQUEST[inteligenciaE]','$_REQUEST[candidatoU]','$_REQUEST[explicacion]','$_REQUEST[resolucionProblemas]','$_REQUEST[observaciones]','$_REQUEST[adecuado]','$_REQUEST[adecuadoP]','$_REQUEST[parcialmente]','$_REQUEST[primera]','$_REQUEST[segunda]','$_REQUEST[noA]','$_REQUEST[concepto]'";
+
+echo $sql;
+if ($objCnx->query($sql)) {
+	alert("Se hizo el registro correctamente");
+	header("location:../View/insertar.php?pag=insertar&msj=1");
+	mysqli_close($objCnx);
+}else{
+alert("no se pudo hacer el registro "+$sql);
+header("location:../View/insertar.php?pag=insertar&msj=2");
+	mysqli_close($objCnx);
+}
+ ?>
