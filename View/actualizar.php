9a4f5cb23dc7160364e6c6864991c9736ec946be
karenjuliana <karengomezmattos@gmail.com>
2020-04-16 04:03:17 -0500

    Diseño y Ojitoo

--- View/actualizar.php
+++ View/actualizar.php
@@ -8,42 +8,47 @@ sessionValidate();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <?php include FOLDER_TEMPLATE."head.php"; ?>
 <?php include FOLDER_TEMPLATE."sidebar.php"; ?>
-<body>          
-          
-<div class="container">
-    <div class="card o-hidden border-0 shadow-lg my-5">
-      <div class="card-body p-0 bg-gray-300"> 
 
-        <!-- /. NAV TOP  -->
-      
-        <!-- /. NAV SIDE  -->
-        <div id="page-wrapper" >
-            <div id="page-inner">
-                <div class="row">
-                    <div class="col-md-12">
-                    <div class="row" align="center"> 
-                    <div class="form-group">
-                      <br><br>
-                      <form id="form2" name="form1" method="post" action="actualizar.php" class="form-group">
-                        <table class="table table-striped table-bordered table-hover" width="42%" border="0" align="center">
-                          <tr>
-                            <td colspan="2" align="center">ACTUALIZAR EMPLEADO</td>
-                          </tr>
-                          <tr>
-                            <td width="28%" align="right" bgcolor=" #ffae00">BUSCAR</td>
-                            <td><label for="co"></label>
-                              <?php $co=0;
+<body>
+
+    <div class="container">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0 bg-gray-300">
+
+                <!-- /. NAV TOP  -->
+
+                <!-- /. NAV SIDE  -->
+                <div id="page-wrapper">
+                    <div id="page-inner">
+                        <div class="col-md-8 mx-auto">
+                            <div class="form-group">
+                                <br>
+                                <form id="form2" name="form1" method="post" action="actualizar.php" class="form-group">
+                                    <table class="table table-striped table-bordered table-hover" width="42%" border="0"
+                                        align="center">
+                                        <tr>
+                                            <td colspan="2" align="center"><b>ACTUALIZAR EMPLEADO</b></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="18%" align="right">
+                                                <input type="submit" class="btn btn-success mt-4 class=" form-control"
+                                                    name="Consultar" value="Consultar">
+                                            </td>
+                                            <td><label for="co"></label>
+                                                <?php $co=0;
                               $nombre="";
                               $apellidos="";
                               $Perfil="";
                               $correo_electronico="";
                               $contraseña="";
                               ?>
-                              <input type="text" name="Buscar" id="Buscar" class="form-control" size="40" value="<?php echo $co;?>" required/>
-                              <input type="submit" class="btn btn-warning" class="form-control"  name="Consultar" value="Consultar">
+                                                <input type="text" name="Buscar" id="Buscar"
+                                                    class="form-control form-control-user mb-2" size="40"
+                                                    value="<?php echo $co;?>" required />
 
-                            </td>
-                            <?php 
+
+                                            </td>
+                                            <?php 
 
                             $co="";
                             if (isset($_POST["Consultar"])) {
@@ -64,52 +69,88 @@ sessionValidate();
                               }
                             }
                              ?>
-                          </tr>
+                                        </tr>
+                                    </table>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="col-md-8 ml-5">
+                            <div class="form-group">
+                                <br>
+                                <div class="row">
+                                    <form id="form1" name="form1" method="post"
+                                        action="../Controller/ValidarActualizarUsuario.php">
+                                        <table class="table table-striped table-bordered table-hover " width="42%"
+                                            align="center">
+                                            <tr>
+                                                <td align="right" bgcolor="transparent"><b>Nombre</b></td>
+                                                <td>
+                                                    <input type="text" name="nombre" id="nombre"
+                                                        class="form-control form-control-user "
+                                                        value="<?php echo $nombre;?>" style="width: 320px" size="40"
+                                                        required /></td>
+
+                                                <td height="25" align="right" bgcolor="transparent"><b>Perfil</b></td>
+                                                <td>
+                                                    <input type="text" name="Perfil" id="Perfil"
+                                                        class="form-control form-control-user "
+                                                        value="<?php echo $Perfil;?>" size="40" required />
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="right" bgcolor="transparent"><b>Apellido</b> </td>
+                                                <td><input type="text" name="apellidos" id="apellidos"
+                                                        class="form-control form-control-user"
+                                                        value="<?php echo $apellidos;?>" style="width: 320px" size="40"
+                                                        required /></td>
+                                                <td align="right" bgcolor="transparent"><b>Correo Electrónico<b></td>
+                                                <td><input type="email" name="correo_electronico"
+                                                        id="correo_electronico" class="form-control form-control-user"
+                                                        value="<?php echo $correo_electronico;?>" size="40" required />
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td height="25" align="right" bgcolor="transparent"><b>CO</b></td>
+                                                <td><input type="text" name="co" id="co" class="form-control"
+                                                        value="<?php echo $co;?>" style="width: 320px" size="40"
+                                                        required /></td>
+
+                                                <td height="25" align="right" bgcolor="transparent"><b>Contraseña</b>
+                                                </td>
+                                                <td><input type="text" name="contraseña" id="contraseña"
+                                                        class="form-control form-control-user"
+                                                        value="<?php echo $contraseña;?>" style="width: 320px"
+                                                        required /></td>
+                                            </tr>
+                                        </table>
+
+                                        <div class="row justify-content-center">
+                                            <input class="btn btn-primary mt-2 type=" submit" name="button" id="button"
+                                                value="ACTUALIZAR">
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <table class="table table-striped table-bordered table-hover" width="42%" border="0"
+                            align="center">
+                            <tr>
+                                <td colspan="2" align="center">
+                                    <h1>Listado de Usuarios</h1>
+                                </td>
+                            </tr>
                         </table>
-                      </form>
-                      <form id="form1" name="form1" method="post" action="../Controller/ValidarActualizarUsuario.php">
-                        <table class="table table-striped table-bordered table-hover" width="42%" align="center">
-                        <tr>
-                          <td width="28%" align="right" bgcolor="#ffae00">Nombre</td>
-                          <td width="72%"> <label for="co"></label>
-                          <input type="text" name="nombre" id="nombre" value="<?php echo $nombre;?>" size="40" required/></td>
-                        </tr>
-                        <tr>
-                          <td  align="right" bgcolor="#ffae00">Apellido</td>
-                          <td><input type="text" name="apellidos" id="apellidos" value="<?php echo $apellidos;?>" size="40" required/></td>
-                        </tr>
-                        <tr>
-                          <td height="25" align="right" bgcolor="#ffae00">CO</td>
-                          <td><input type="text" name="co" id="co" value="<?php echo $co;?>" size="40" required/></td>
-                        </tr>
-                        <tr>
-                          <td height="25" align="right" bgcolor="#ffae00">Perfil</td>
-                          <td><input type="text" name="Perfil" id="Perfil" value="<?php echo $Perfil;?>" size="40" required/></td>
-                        </tr>
-                        <tr>
-                          <td  align="right" bgcolor="#ffae00">Correo Electronico</td>
-                          <td><input type="email" name="correo_electronico" id="correo_electronico" value="<?php echo $correo_electronico;?>" size="40" required/></td>
-                        </tr>
-                        <tr>
-                          <td  align="right" bgcolor="#ffae00">Contraseña</td>
-                          <td><input type="text" name="contraseña" id="contraseña" value="<?php echo $contraseña;?>" style="width: 270px"required/></td>
-                        </tr>
-                        <tr>
-                          <td colspan="2" align="center" bgcolor="#fffff"><input  class="btn btn-warning" type="submit" name="button" id="button" value="ACTUALIZAR"></td>
-                        </tr>
-                        </table>
-                      </form>
-                      <h1>Listado de Usuarios</h1>
-                      <table class="table table-striped table-bordered table-hover" width="70%" border="1" align="center">
-                      <tr align="center" bgcolor="#ECAB97">
-                          <td>Nombre</td>
-                          <td>Apellido</td>
-                          <td>CO</td>
-                          <td>Perfil</td>
-                          <td>Correo Electronico</td>
-                          <td>Contraseña</td>
-                      </tr>
-                      <?php 
+                        <table class="table table-striped table-bordered table-hover" width="70%" border="1"
+                            align="center">
+                            <tr align="center" bgcolor="#ECAB97">
+                                <td>Nombre</td>
+                                <td>Apellido</td>
+                                <td>CO</td>
+                                <td>Perfil</td>
+                                <td>Correo Electronico</td>
+                                <td>Contraseña</td>
+                            </tr>
+                            <?php 
                             $co="";
                             if (isset($_POST["Consultar"])) {
                               $co=$_POST["Buscar"];
@@ -121,20 +162,20 @@ sessionValidate();
 
                               while ($datos=$rta->fetch_array()) {
                         ?>
-                              <tr>
+                            <tr>
                                 <td><?php echo $datos['nombre'];?> </td>
                                 <td><?php echo $datos['apellidos'];?></td>
                                 <td><?php echo $datos['co'];?></td>
                                 <td><?php echo $datos['Perfil'];?></td>
                                 <td><?php echo $datos['correo_electronico'];?></td>
                                 <td><?php echo $datos['contraseña'];?></td>
-                              </tr>
-                              <?php
+                            </tr>
+                            <?php
                               }
                             }
                             ?>
-                      </table>
-                      <?php 
+                        </table>
+                        <?php 
 
                       extract($_REQUEST);
 
@@ -147,12 +188,13 @@ sessionValidate();
                         }
                       }
                        ?>
-</div>
-</div>          
-    </div>
+
+                    </div>
+                </div>
             </div>
-        </div> 
-<?php include FOLDER_TEMPLATE. "footer.php"; ?>
-<?php include FOLDER_TEMPLATE. "scripts.php"; ?>  
+        </div>
+        <?php include FOLDER_TEMPLATE. "footer.php"; ?>
+        <?php include FOLDER_TEMPLATE. "scripts.php"; ?>
 </body>
+
 </html>
--- View/eliminar.php
+++ View/eliminar.php
@@ -1,29 +1,33 @@
   <?php include"../config.php";
 sessionValidate();
 ?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<?php include FOLDER_TEMPLATE."head.php"; ?>
-<?php include FOLDER_TEMPLATE."sidebar.php"; ?>
-<body>      
-  
-<div class="container">
-    <div class="card o-hidden border-0 shadow-lg my-5">
-      <div class="card-body p-0 bg-gray-300"> 
-                <div class="row">
-                    <div class="col-md-12">
-                    <div class="row" align="center"> 
-                    <div class="form-group" ">
-                      <br><br>
-                      <form id="form2" name="form1" method="post" action="eliminar.php" class="form-group">
-                        <table class="table table-striped table-bordered table-hover" width="42%" border="0" align="center">
-                          <tr>
-                            <td colspan="2" align="center">ELIMINAR USUARIO</td>
-                          </tr>
-                          <tr>
-                            <td width="28%" align="right" bgcolor="#ffae00">BUSCAR</td>
-                            <td><label for="co"></label>
-                              <?php 
+  <!DOCTYPE html>
+  <html xmlns="http://www.w3.org/1999/xhtml">
+  <?php include FOLDER_TEMPLATE."head.php"; ?>
+  <?php include FOLDER_TEMPLATE."sidebar.php"; ?>
+
+  <body>
+
+      <div class="container">
+          <div class="card o-hidden border-0 shadow-lg my-5">
+              <div class="card-body p-0 bg-gray-300">
+                  <div class="row">
+                      <div class="col-md-12">
+                          <div class="col-md-8 mx-auto">
+                              <div class="form-group">
+                                  <br>
+                                  <form id="form2" name="form1" method="post" action="eliminar.php" class="form-group">
+                                      <table class="table table-striped table-bordered table-hover" width="42%"
+                                          border="0" align="center">
+                                          <tr>
+                                              <td colspan="2" align="center"><b>ELIMINAR USUARIO</b></td>
+                                          </tr>
+                                          <tr>
+                                              <td align="center"> <input type="submit"
+                                                      class="btn btn-success " name="Consultar" value="CONSULTAR">
+                                              </td>
+                                              <td>
+                                                  <?php 
                               $nombre="";
                               $apellidos="";
                               $co=0;
@@ -31,10 +35,12 @@ sessionValidate();
                               $correo_electronico="";
                               $contraseña="";
                                ?>
-                              <input type="text" name="Buscar" id="Buscar" size="40" value="<?php echo $co;?>" required/>
-                              <input type="submit" class="btn btn-warning" name="Consultar" value="Consultar">
-                            </td>
-                            <?php 
+                                                  <input type="number" name="Buscar" id="Buscar"
+                                                      class="form-control form-control-user mb-2 mr-5" size="40"
+                                                      value="<?php echo $co;?>" required />
+
+                                              </td>
+                                              <?php 
 
                             if (isset($_POST["Consultar"])) {
                               $co=$_POST["Buscar"];
@@ -54,21 +60,44 @@ sessionValidate();
                               }
                             }
                              ?>
-                          </tr>
-                        </table>
-                      </form>
-                      <form id="form1" name="form1" method="post" action="../Controller/ValidarEliminarEmpleado.php">
-                        <h1>Listado de Usuarios</h1>
-                      <table class="table table-striped table-bordered table-hover" width="70%" border="1" align="center">
-                      <tr align="center" bgcolor="#ECAB97">
-                          <td>Nombre</td>
-                          <td>Apellido</td>
-                          <td>Co</td>
-                          <td>Perfil</td>
-                          <td>Correo Electronico</td>
-                          <td>Contraseña</td> 
-                      </tr>
-                      <?php 
+                                          </tr>
+                                          <tr align="center" bgcolor="#CFE0EE">
+          <td> <input class="btn btn-danger mt-3" type="submit" name="button" id="button"
+     value="ELIMINAR">
+          </td>
+          <td colspan="2" align="center" > Identificación a Eliminar
+              <input type="number" name="co" id="co" readonly="readonly" size="40"
+                  value="<?php echo $co;?>" class="form-control form-control-user " required />
+             
+             
+          </td>
+      </tr>
+  </table>
+                                      </table>
+                                  </form>
+                              </div>
+                          </div>
+                          <form id="form1" name="form1" method="post"
+                              action="../Controller/ValidarEliminarEmpleado.php">
+                              <table class="table table-striped table-bordered table-hover" width="42%" border="0"
+                                  align="center">
+                                  <tr>
+                                      <td colspan="2" align="center">
+                                          <h1>Listado de Usuarios</h1>
+                                      </td>
+                                  </tr>
+                              </table>
+                              <table class="table table-striped table-bordered table-hover" width="70%" border="1"
+                                  align="center">
+                                  <tr align="center" bgcolor="#ECAB97">
+                                      <td>Nombre</td>
+                                      <td>Apellido</td>
+                                      <td>Co</td>
+                                      <td>Perfil</td>
+                                      <td>Correo Electronico</td>
+                                      <td>Contraseña</td>
+                                  </tr>
+                                  <?php 
 
                             if (isset($_POST["Consultar"])) {
                               $co=$_POST["Buscar"];
@@ -80,27 +109,19 @@ sessionValidate();
 
                               while ($datos=$rta->fetch_array()) {
                         ?>
-                              <tr>
-                                <td><?php echo $datos['nombre'];?></td> 
-                                <td><?php echo $datos['apellidos'];?></td>
-                                <td><?php echo $datos['co'];?></td>
-                                <td><?php echo $datos['Perfil'];?></td>
-                                <td><?php echo $datos['correo_electronico'];?></td>
-                                <td><?php echo $datos['contraseña'];?></td> 
-                              </tr>
-                              <?php
+                                  <tr>
+                                      <td><?php echo $datos['nombre'];?></td>
+                                      <td><?php echo $datos['apellidos'];?></td>
+                                      <td><?php echo $datos['co'];?></td>
+                                      <td><?php echo $datos['Perfil'];?></td>
+                                      <td><?php echo $datos['correo_electronico'];?></td>
+                                      <td><?php echo $datos['contraseña'];?></td>
+                                  </tr>
+                                  <?php
                               }
                             }
                             ?>
-                      <tr align="center" bgcolor="#CFE0EE">
-                        <td>Identificacion a Eliminar</td>
-                          <td colspan="2" align="center" bgcolor="#FFFFFF">
-                            <input type="number" name="co" id="co" readonly="readonly" size="40" value="<?php echo $co;?>" required/>
-                            <input  class="btn btn-warning" type="submit" name="button" id="button" value="ELIMINAR">
-                          </td>
-                      </tr>
-                      </table>
-                      <?php 
+                              <?php 
 
                       extract($_REQUEST);
 
@@ -113,14 +134,15 @@ sessionValidate();
                         }
                       }
                        ?>
-                      </form>
-</div>
-</div>         
-    </div>
-            </div>
-        </div> 
-                    </div>
-<?php include FOLDER_TEMPLATE. "footer.php"; ?>
-<?php include FOLDER_TEMPLATE. "scripts.php"; ?>   
-</body>
-</html>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      </div>
+      <?php include FOLDER_TEMPLATE. "footer.php"; ?>
+      <?php include FOLDER_TEMPLATE. "scripts.php"; ?>
+  </body>
+
+  </html>
